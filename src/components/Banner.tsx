7ed3bc2e8b244ada70ee3ad14399b9844812f4f9
Your Name <tangshirong477@gmail.com>
2025-08-17 07:00:40 -0400

    dropdown animation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='bg-[#bcc5bc] p-2 flex justify-center'>
+    <div className='bg-[#86A788] p-2 flex justify-center'>
         <a 
           href="#" 
           className="
-            font-normal text-[#2c362d] font-sm
+            font-normal text-[#151a16] font-sm
             hover:pointer hover:text-[#617963]
             transition-colors duration-300 ease-in-out
             opacity-0 animate-[fadeIn_1s_ease-in_forwards]">
-            10% discount for new customer →
+          10% discount for new customer →
         </a>
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 import { FiMenu } from "react-icons/fi";
+import { RiArrowDownWideFill } from "react-icons/ri";
 
 const Navbar = () => {
-const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true);
+  const [open,setOpen] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-    const threshold = window.innerHeight * 0.4
+    const threshold = window.innerHeight * 0.5
       if (window.scrollY > lastScrollY && window.scrollY > threshold) {
         // scrolling down hide
         setShow(false);
@@ -22,12 +24,15 @@ const [show, setShow] = useState(true);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
+
+
   return (
     <nav
-        className={`sticky top-0 left-0 w-full bg-white shadow-md transition-transform duration-300 z-50 ${show ? "translate-y-0" : "-translate-y-full"
+        className={`sticky top-0 left-0 w-full bg-white shadow-sm transition-transform duration-300 z-50 ${show ? "translate-y-0" : "-translate-y-full"
             }`}
     >
 
+        {/* Navbar */}
         <div
             id='navbar'
             className={`bg-[#f8f8f8] p-4 flex justify-between shadow-sm shadow-gray-100`}>
@@ -41,10 +46,23 @@ const [show, setShow] = useState(true);
                 className='flex items-center'>
                 <a
                     href="#"
+                    onClick={() => setOpen(!open)}
                     className='text-[#2c362d] font-sm hover:text-[#617963] transition-colors duration-300 ease-in-out'>
-                    <FiMenu size={24} />
+                    {open? <RiArrowDownWideFill size={24} />: <FiMenu size={24} />}
+
                 </a>
             </div>
+        </div>
+
+        {/* Dropdown - suitable for desktop*/}
+        <div
+          className={`w-full overflow-hidden transition-all duration-500 ease-in-out bg-[#f8f8f8] flex items-center justify-center text-black text-normal font-normal ${open ? 'max-h-64 py-5 opacity-100' : 'max-h-0 py-0 opacity-0'}`}
+        >
+            <ul className='w-full flex flex-col'>
+                <li className="menu-item">Home</li>
+                <li className="menu-item">Products</li>
+                <li className="menu-item">Activities</li>
+            </ul>
         </div>
 
     </nav>
