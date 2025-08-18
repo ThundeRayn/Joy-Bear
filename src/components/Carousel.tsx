c1d9f53ff44d29b52facc3817e43dcd55afe4c33
Your Name <tangshirong477@gmail.com>
2025-08-18 02:31:20 -0400

    before changing the carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import Bear1 from '../assets/imgs/bear1.webp'
-import Bear2 from '../assets/imgs/bear2.webp'
-import Bear3 from '../assets/imgs/bear3.webp'
+import { RiArrowLeftWideFill } from "react-icons/ri";
+import { RiArrowRightWideFill } from "react-icons/ri";
+
+import Bear1 from '../assets/imgs/bear1_11zon.webp'
+import Bear2 from '../assets/imgs/bear2_11zon.webp'
+import Bear3 from '../assets/imgs/bear3_11zon.webp'
 
 const Activities = [
   { id: 1, image: Bear2, title: 'Item 1' },
   { id: 2, image: Bear1, title: 'Item 2' },
   { id: 3, image: Bear3, title: 'Item 3' },
-  { id: 4, image: Bear3, title: 'Item 4' },
 ]
 
 const Carousel = () => {
@@ -25,26 +27,27 @@ const Carousel = () => {
   return (
     <div className="w-full h-[50vh] md:h-[70vh] lg:h-[85vh] overflow-hidden relative">
         <div 
-          className="flex transition-transform duration-500 h-full"
+          className="flex transition-transform duration-300 h-full"
           style={{ transform: `translateX(-${current * 100}%)` }}
         >
             {Activities.map((activity, index) => (
             <img 
               key={index}
               src={activity.image} 
+              loading="lazy"
               alt={activity.title} 
               className="w-full h-full object-cover object-center flex-shrink-0"/>
             ))}
         </div>
 
         <button
-          className="absolute left-4 top-1/2 -translate-y-1/2 z-10 p-2 text-white bg-black bg-opacity-50 rounded-full"
+          className="absolute left-4 top-1/2 -translate-y-1/2 z-10 p-2 text-[#2c362d] bg-white bg-opacity-50 rounded-full cursor-pointer"
           onClick={prevSlide}
-        >Previous</button>
+        ><RiArrowLeftWideFill /></button>
         <button
-          className="absolute right-4 top-1/2 -translate-y-1/2 z-10 p-2 text-white bg-black bg-opacity-50 rounded-full"
+          className="absolute right-4 top-1/2 -translate-y-1/2 z-10 p-2 text-[#2c362d] bg-white bg-opacity-50 rounded-full cursor-pointer"
           onClick={nextSlide}
-        >Next</button>
+        ><RiArrowRightWideFill /></button>
     </div>
   )
 }
