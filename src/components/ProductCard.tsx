d45b7b2d46f9be1b794e505fe8d8872718e3a9dc
Your Name <tangshirong477@gmail.com>
2025-08-30 02:34:49 -0400

    img displayed

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -31,7 +31,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                     {true && (
                         <div className="relative h-64 w-full">
                         <img
-                            src={`https://images.unsplash.com/photo-1583478415880-b79447d73a84?q=80&w=1112&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
+                            src={product.images && product.images.length > 0 ? `${product.images[0]}` : `https://images.unsplash.com/photo-1583478415880-b79447d73a84?q=80&w=1112&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
                             alt={product.title}
                             className="object-cover w-full h-full"
                         />
