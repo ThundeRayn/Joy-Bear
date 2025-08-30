6ec3b334d5b54c1980b7730b7a33a6c3d659abb7
Your Name <tangshirong477@gmail.com>
2025-08-29 22:08:19 -0400

    error page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from "./components/Layout";
 import Home from './pages/Home'
 import Product from './pages/Product'
+import ProductDetail from './pages/ProductDetail';
+import Error from './pages/Error';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<Product />} />
+          <Route path="products/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Error.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Error: React.FC = () => {
+  return (
+    <div className="flex h-150 w-full flex-col items-center justify-center bg-gray-100 text-center">
+      <div className="text-7xl text-gray-400">🐻</div>
+      <h1 className="mt-6 text-2xl font-semibold text-gray-700">
+        Oops, this page is not found.
+      </h1>
+      <button className="mt-8">
+        <a href="/" className="inline-block bg-[#86A788] text-white px-6 py-3 rounded-xl shadow hover:bg-[#6a886c] transition">
+          Back to Home
+        </a>
+      </button>
+    </div>
+  );
+};
+
+export default Error
