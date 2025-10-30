c9021ddc53bec3b2b856503bf7f425093d0bec29
Your Name <tangshirong477@gmail.com>
2025-10-30 16:15:55 -0400

    customize page scratch

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ProductDetail from './pages/ProductDetail';
 import Error from './pages/Error';
 import About from './pages/About';
 import Story from './pages/Story';
+import Cutomize from './pages/Cutomize';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="products" element={<Product />} />
           <Route path="products/:slug" element={<ProductDetail />} />
           <Route path="story" element={<Story />} />
+          <Route path="customize" element={<Cutomize />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
