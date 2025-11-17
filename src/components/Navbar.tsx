fcaacce3841aa78c79e491ca9c85fc3c464fde9a
Your Name <tangshirong477@gmail.com>
2025-11-17 00:00:52 -0500

    navbar adjust

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,37 +39,46 @@ const Navbar = () => {
             className={`bg-[#f8f8f8] shadow-gray-100`}>
 
           {/* Desktop: Logo centered */}
-          <div className='hidden md:flex justify-center items-center p-4'>
+          <div className='hidden md:flex justify-center items-center py-1 border-b border-b-gray-200'>
             <div className="w-full max-w-6xl mx-auto flex items-center">
 
-              <div className="flex-1 flex justify-start">
+              {/* <div className="flex-1 flex justify-start">
                 <div className="w-full max-w-xs">
                   <Search />
                 </div>
-              </div>
+              </div> */}
 
               <div className="flex-1 flex justify-center">
                 <a href='/'>
                   <div id='nav-logo' className='flex justify-center items-center gap-2'>
-                      <img src="/joybear-icononly.svg" alt="Joy Bear Logo" className="w-16 h-16 inline" />
+                      <img src="/joybear-icononly.svg" alt="Joy Bear Logo" className="w-18 h-18 inline" />
                   </div>
                 </a>
               </div>
 
-              <div className="flex-1 flex justify-end">
+              {/* <div className="flex-1 flex justify-end">
                 <a
                   href="mailto:hello@joybear.com"
                   className="inline-block px-6 py-2 rounded-xl border border-Joybrown text-Joybrown font-medium transition-colors duration-200 hover:text-[#8b6f47] hover:border-[#8b6f47] whitespace-nowrap"
                 >
                   Contact Us Now
                 </a>
-              </div>
+              </div> */}
             </div>
           </div>
 
-          {/* Desktop: Menu centered */}
-          <div className='hidden md:flex justify-center items-center pb-4'>
-            <ul className='flex items-center gap-6'>
+
+          {/* Desktop: Menu with left, center, right layout */}
+          <div id='menu' className='hidden md:flex items-center justify-between p-2 w-full max-w-6xl mx-auto'>
+            {/* Search on left */}
+            <div className="flex-1 flex justify-start">
+              <div className="w-2/3 max-w-xs">
+                <Search />
+              </div>
+            </div>
+
+            {/* Menu items centered */}
+            <ul className='flex flex-row items-center gap-6 justify-center'>
               <li>
                 <a href='/products' className="text-[#2c362d] hover:text-txt-secondary transition-colors duration-300 ease-in-out">EXPLORE ALL</a>
               </li>
@@ -86,6 +95,16 @@ const Navbar = () => {
                 <a href='/customize' className="text-[#2c362d] hover:text-txt-secondary transition-colors duration-300 ease-in-out">CUSTOMIZE-ODM</a>
               </li>
             </ul>
+
+            {/* Contact us on right */}
+            <div className="flex-1 flex justify-end">
+              <a
+                href="mailto:hello@joybear.com"
+                className="inline-block px-6 py-2 rounded-xl border border-Joybrown text-Joybrown font-medium transition-colors duration-200 hover:text-[#8b6f47] hover:border-[#8b6f47] whitespace-nowrap"
+              >
+                Contact Us Now
+              </a>
+            </div>
           </div>
 
           {/* Mobile: Logo and Menu toggle side by side */}
